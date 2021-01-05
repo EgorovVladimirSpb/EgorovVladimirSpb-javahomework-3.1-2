31b66bcd5c405b621238d699cf2b362ddd2ed1cd
Egorov Vladimir <egorov.vladimir.spb@gmail.com>
2021-01-05 10:43:24 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+
+    private String commentsReorder;
+    private String commentAuthor;
+    private String commentAuthorPhotoUrl;
+    private String commentText;
+    private int commentDate;
+    private boolean canComment;
+    private boolean canAddImages;
+    private boolean canAddAudios;
+    private boolean canAddVideos;
+
+    // + getters/setters*
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,23 @@
+package ru.netology.domain;
+
+public class Post {
+
+    private String mediaLinkPhoto;
+    private String mediaLinkSubtitle;
+    private String mediaLinkTitle;
+    private String postText;
+    private int postDate;
+    private String postAuthor;
+    private Boolean postFixed;
+    private String menuAction;
+    private int viewsInfo;
+    private int repostsInfo;
+    private int likesInfo;
+    private boolean canComment;
+    private boolean canRepost;
+    private boolean canLike;
+    private CommentsInfo commentsInfo;
+
+
+    // + getters/setters*
+}
